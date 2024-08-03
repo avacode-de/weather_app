4119617dd80276c59891ef707c93020f90db50c5
gigaunt.jn@gmail.com <avacode-de>
2024-08-03 20:06:36 +0300

    fix crud

--- app/crud.py
+++ app/crud.py
@@ -3,8 +3,18 @@ from .config import WEATHER_API_KEY, WEATHER_API_URL
 from .models import WeatherResponse
 
 def get_weather(city: str) -> WeatherResponse:
-    response = requests.get(WEATHER_API_URL, params={"q": city, "appid": WEATHER_API_KEY, "units": "metric"})
+    response = requests.get(
+        WEATHER_API_URL,
+        params={
+            "q": city,
+            "appid": WEATHER_API_KEY,
+            "units": "metric"
+        }
+    )
     data = response.json()
+    if response.status_code != 200:
+        raise Exception(data.get("message", "Error fetching weather data"))
+    
     return WeatherResponse(
         temperature=data["main"]["temp"],
         description=data["weather"][0]["description"],
